eaf1a00b8fbb1ce619fececb28f27228939c4013
Mike de Sousa <mikecostanascimento@gmail.com>
2021-12-02 15:33:54 -0300

    Construtores + Métodos Estáticos

---
+++ src/beans/Produto.java
@@ -0,0 +1,69 @@
+package beans;
+
+public class Produto {
+
+	private String nome;
+	private String marca;
+	private float valor;
+	private boolean promocao;
+
+	public Produto () {
+		
+	}
+
+	// Construtor
+	
+	public Produto(String nome, String marca, float valor) {
+	this.nome = nome;
+	this.marca = marca;
+	this.valor = valor;	
+		
+	}
+	
+	public Produto(String nome, String marca, float valor, boolean promocao) {
+		this.nome = nome;
+		this.marca = marca;
+		this.valor = valor;
+		this.promocao = promocao;
+	}
+	
+	// Getters + Setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public boolean isPromocao() {
+		return promocao;
+	}
+
+	public void setPromocao(boolean promocao) {
+		this.promocao = promocao;
+	}
+
+	// Métodos + Operações
+	public String Detalhes() {
+		return "Produto :" + this.nome + " valor: " + this.valor;
+
+	}
+
+}
---
+++ src/implementacao/ImplementarProduto2.java
@@ -0,0 +1,50 @@
+package implementacao;
+
+import javax.swing.JOptionPane;
+
+import beans.Produto;
+
+public class ImplementarProduto2 {
+
+	public static void main(String[] args) {
+		
+		Produto impressora = new Produto();
+		
+		impressora. setMarca("HP");
+		String texto = JOptionPane.showInputDialog("Marca");
+		System.out.println("Valor TEXTO: " + texto);
+		
+		impressora.setMarca(JOptionPane.showInputDialog("Marca"));
+		impressora.setNome(JOptionPane.showInputDialog("Descrição"));
+		impressora.setValor(Float.parseFloat(JOptionPane.showInputDialog("Valor: ")));
+		impressora.setPromocao(false);
+		System.out.println(impressora.Detalhes());
+		
+		JOptionPane.showConfirmDialog(null, "Promoção? ", "Informe", JOptionPane.YES_NO_OPTION);
+		
+		int resp = JOptionPane.showConfirmDialog(null, "Promoção? ", "Informe", JOptionPane.YES_NO_OPTION);
+		
+		if (resp == 0) {
+			impressora.setPromocao(true);
+		}else {
+			impressora.setPromocao(false);
+		}
+		
+		System.out.println(impressora.Detalhes() + " Promoção: " + impressora.isPromocao());
+		
+		Produto p2 = new Produto(JOptionPane.showInputDialog(" Descrição"),
+				                 JOptionPane.showInputDialog(" Marca"), 
+				                 Float.parseFloat(JOptionPane.showInputDialog(" Valor: ")));
+		System.out.println(p2.Detalhes());
+		
+		Produto p3 = new Produto(JOptionPane.showInputDialog(" Descrição"),
+                JOptionPane.showInputDialog(" Marca"), 
+                Float.parseFloat(JOptionPane.showInputDialog(" Valor: ")),
+                Boolean.parseBoolean(JOptionPane.showInputDialog(" Promoção ? ")));
+        System.out.println(p3.Detalhes() + p3.isPromocao());
+		
+		
+
+	}
+
+}
